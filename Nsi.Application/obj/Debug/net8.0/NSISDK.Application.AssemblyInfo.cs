87c895d4a1d1c37919529b0a965b4c3fbb344d96
Purgenta <onlinenidza352@gmail.com>
2024-04-30 13:55:59 +0200

    Updated packages/structure

--- Nsi.Application/obj/Debug/net8.0/NSISDK.Application.AssemblyInfo.cs
+++ Nsi.Application/obj/Debug/net8.0/NSISDK.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NSISDK.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+476aea8a7b385e8882ccd4b542b2f04530b1f765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4a022828e313adb6859d1fd6d7629d961a438f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("NSISDK.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NSISDK.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
